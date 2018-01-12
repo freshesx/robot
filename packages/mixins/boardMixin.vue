0c8157a4af27c16eb6a12016f809e0d942e9fb14
ChaselWu <minowu@foxmail.com>
2018-01-12 23:31:48 +0800

    Update boardMixin

--- packages/mixins/boardMixin.vue
+++ packages/mixins/boardMixin.vue
@@ -1,9 +1,62 @@
 <template>
-  <router-view></router-view>
+  <div class="rx-layout">
+    <div class="rx-layout-aside">
+      <rx-routes :routes="routes"/>
+    </div>
+    <div class="rx-layout-container">
+      <div class="rx-layout-header"></div>
+      <div class="rx-layout-main">
+        <router-view></router-view>
+      </div>
+      <div class="rx-layout-footer"></div>
+    </div>
+  </div>
 </template>
 
 <script>
+import { RxRoutes } from '../routes'
+
 export default {
-  name: 'rx-layout-mixin'
+  name: 'rx-layout-mixin',
+  components: {
+    RxRoutes
+  },
+  data () {
+    return {
+      routes: []
+    }
+  }
 }
 </script>
+
+<style lang="scss">
+.rx-layout {
+  display: flex;
+  width: 100vw;
+  height: 100vh;
+}
+
+.rx-layout-aside {
+  width: 160px + 24px;
+  height: 100vh;
+  background: rgba(black, 0.2);
+}
+
+.rx-layout-container {
+  display: flex;
+  flex-direction: column;
+  flex: 1;
+}
+
+.rx-layout-main {
+  flex: 1;
+}
+
+.rx-layout-header {
+  flex-shrink: 0;
+}
+
+.rx-layout-footer {
+  flex-shrink: 0;
+}
+</style>
