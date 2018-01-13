e1d1b00ef0ec5260407741163f7119bebe1a5dbb
ChaselWu <minowu@foxmail.com>
2018-01-13 15:24:27 +0800

    Update board routes.

--- examples/src/router/board.js
+++ examples/src/router/board.js
@@ -3,33 +3,35 @@ export default [
     path: '/',
     name: 'homepage',
     meta: {
-      title: '首页'
+      title: 'Homepage',
+      nav: true
     },
     component: () => import('../components/home')
   },
   {
     path: 'products',
+    name: 'products',
     meta: {
-      title: '产品'
+      title: 'Products',
+      nav: true
     },
-    component: () => import('../components/products'),
-    children: [
-      {
-        path: '/',
-        name: 'products.list',
-        meta: {
-          title: '产品列表'
-        },
-        component: () => import('../components/products/list.vue')
-      },
-      {
-        path: ':productId/edit',
-        name: 'products.edit',
-        meta: {
-          title: '编辑产品'
-        },
-        component: () => import('../components/products/edit.vue')
-      }
-    ]
+    component: () => import('../components/products/index.vue')
+  },
+  {
+    path: 'products/:productId/edit',
+    name: 'products.edit',
+    meta: {
+      title: 'Edit Product'
+    },
+    component: () => import('../components/products/edit.vue')
+  },
+  {
+    path: 'product-categories',
+    name: 'productCategories',
+    meta: {
+      title: 'Product Categories',
+      nav: true
+    },
+    component: () => import('../components/product-categories/index.vue')
   }
 ]
