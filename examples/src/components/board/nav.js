25cf78077a05926dc3ea28d4c93243d810e34c64
ChaselWu <minowu@foxmail.com>
2018-01-13 15:24:43 +0800

    Add nav

---
+++ examples/src/components/board/nav.js
@@ -0,0 +1,19 @@
+export default [
+  {
+    title: 'Home',
+    name: 'homepage'
+  },
+  {
+    title: 'Products',
+    children: [
+      {
+        title: 'products',
+        name: 'products'
+      },
+      {
+        title: 'Categories',
+        name: 'productCategories'
+      }
+    ]
+  }
+]
