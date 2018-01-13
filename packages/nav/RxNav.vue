d4ec588df9665a9da34e15d09c6137b7acfe45b5
ChaselWu <minowu@foxmail.com>
2018-01-13 14:25:49 +0800

    Some RxNav and RxNavItem.

--- packages/nav/RxNav.vue
+++ packages/nav/RxNav.vue
@@ -2,19 +2,31 @@
   <div class="rx-nav">
     <h1>rx-nav-bar</h1>
     <div class="rx-nav-items">
-
+      <rx-nav-item
+        :item="route"
+        v-for="(route, key) in filterNavRoutes(routes)" :key="key"
+      />
     </div>
   </div>
 </template>
 
 <script>
+import RxNavItem from './RxNavItem'
+import { filterNavRoutes } from './helpers'
+
 export default {
   name: 'RxNav',
+  components: {
+    RxNavItem
+  },
   props: {
     routes: {
       type: Array,
       default: () => []
     }
+  },
+  methods: {
+    filterNavRoutes
   }
 }
 </script>
---
+++ packages/nav/RxNavItem.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="rx-nav-item">
+    {{ item.meta.title }}
+    <rx-nav-item
+      :item="route"
+      v-for="(route, key) in filterNavRoutes(item.children)" :key="key"
+    />
+  </div>
+</template>
+
+<script>
+import { filterNavRoutes } from './helpers'
+
+export default {
+  name: 'rx-nav-item',
+  props: {
+    item: {
+      type: Object,
+      default: () => ({})
+    }
+  },
+  methods: {
+    filterNavRoutes
+  }
+}
+</script>
