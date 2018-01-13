dfcbb785d09e947194884e41c5bcbf2c4afa1e81
ChaselWu <minowu@foxmail.com>
2018-01-13 16:13:40 +0800

    Add example store.

--- examples/src/main.js
+++ examples/src/main.js
@@ -1,6 +1,7 @@
 import 'babel-polyfill'
 import Vue from 'vue'
 import robotx from './robotx'
+import store from './store'
 import router from './router'
 import vuelidate from './vuelidate'
 import app from './app'
@@ -10,6 +11,7 @@ Vue.config.productionTip = process.env.NODE_ENV !== 'production'
 new Vue({
   el: '#app',
   robotx,
+  store,
   router,
   vuelidate,
   render: h => h(app)
