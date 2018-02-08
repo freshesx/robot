18bbf88491f8641ab3bbf655c4cd3d9d28680880
ChaselWu <minowu@foxmail.com>
2018-02-08 16:12:15 +0800

    Update dashboard mixin

--- src/mixins/dashboard/RtDashboardMixin.vue
+++ src/mixins/dashboard/RtDashboardMixin.vue
@@ -1,9 +1,39 @@
 <template>
-  <div>dashboard</div>
+  <div class="rt-dashboard-mixin">
+    <el-container>
+      <el-aside class="rt-dashboard-mixin-aside" width="200px">
+        <!-- 侧边栏元素 -->
+        <private-aside></private-aside>
+      </el-aside>
+      <el-container>
+        <el-header>Header</el-header>
+        <el-main>Main</el-main>
+        <el-footer>Footer</el-footer>
+      </el-container>
+    </el-container>
+  </div>
 </template>
 
 <script>
+import PrivateAside from './_aside.vue'
+
 export default {
-  name: 'RtDashboardMixin'
+  name: 'RtDashboardMixin',
+  components: {
+    PrivateAside
+  }
 }
 </script>
+
+<style lang="scss">
+.rt-dashboard-mixin {
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+}
+
+.rt-dashboard-mixin-aside {
+  display: flex;
+  flex-direction: column;
+}
+</style>
---
+++ src/mixins/dashboard/_aside.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="rt-dashboard-aside">
+    <div class="rt-dashboard-aside-brand">
+      brand
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  
+}
+</script>
+
+<style lang="scss">
+  .rt-dashboard-aside {
+    flex: 1;
+    background: #fff;
+    border-right: solid 1px #eee;
+  }
+</style>
