fad0072ecb02383830262429868f9229860aff2f
ChaselWu <minowu@foxmail.com>
2018-02-08 13:04:58 +0800

    Update main.js

--- examples/src/main.js
+++ examples/src/main.js
@@ -1,7 +1,12 @@
 import Vue from 'vue'
+import { Robot } from '@freshes/robot'
 import router from './router'
 import store from './store'
 import app from './app'
+import '../../dist/main.css'
+import 'element-ui/lib/theme-chalk/index.css'
+
+Vue.use(new Robot())
 
 Vue.config.productionTip = process.env.NODE_ENV !== 'production'
 
