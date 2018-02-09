a6c3354cbcc7a118df945a0413f204757f0b00cf
ChaselWu <minowu@foxmail.com>
2018-02-09 13:39:30 +0800

    Add jsx support

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,7 +10,10 @@ module.exports = {
     browser: true,
   },
   // https://github.com/feross/standard/blob/master/RULES.md#javascript-standard-style
-  extends: 'standard',
+  extends: [
+    'standard',
+    'standard-jsx'
+  ],
   // required to lint *.vue files
   plugins: [
     'html'
