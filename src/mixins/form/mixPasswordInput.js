f91a2f5e66a45303d479ad88c1fb1ca74675cf55
ChaselWu <minowu@foxmail.com>
2018-02-11 03:58:15 +0800

    Add mixPasswordInput.

---
+++ src/mixins/form/mixPasswordInput.js
@@ -0,0 +1,28 @@
+import mix from '../../helpers/mix.js'
+import {
+  FormItem as ElFormItem,
+  Input as ElInput } from 'element-ui'
+
+const TextInput = {
+  name: 'RtPasswordInput',
+  render (h) {
+    return (
+      <ElFormItem label={this.label}>
+        <ElInput type='password' value={this.value} placeholder={this.placeholder} />
+      </ElFormItem>
+    )
+  },
+  computed: {
+    label () {
+      return '密码'
+    },
+    value () {
+      return undefined
+    },
+    placeholder () {
+      return '点击输入密码'
+    }
+  }
+}
+
+export default config => mix(config, TextInput)
