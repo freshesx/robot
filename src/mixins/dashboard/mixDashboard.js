713bf54ddb41841c850122a60345d1572a5884ad
ChaselWu <minowu@foxmail.com>
2018-02-11 02:41:26 +0800

    Simpilfy file.

--- src/mixins/dashboard/mixDashboard.js
+++ src/mixins/dashboard/mixDashboard.js
@@ -1,6 +1,99 @@
+import Vue from 'vue'
+import Menu from './_menu.js'
 import mixComponents from '../../helpers/mixComponent.js'
-import RtDashboardMixin from './RtDashboardMixin.js'
 
-export default function (config) {
-  return mixComponents(config, RtDashboardMixin)
+const Dashboard = {
+  name: 'RtDashboard',
+  render (h) {
+    const ElContainer = Vue.component('ElContainer')
+    const ElMain = Vue.component('ElMain')
+    const RouterView = Vue.component('router-view')
+
+    return (
+      <div class='rt-dashboard-mixin'>
+        <ElContainer>
+          {this.renderAside(h)}
+          <ElMain>
+            <RouterView />
+          </ElMain>
+        </ElContainer>
+      </div>
+    )
+  },
+  methods: {
+    renderAside (h) {
+      const ElAside = Vue.component('ElAside')
+
+      return (
+        <ElAside class='rt-dashboard-mixin__aside' width={this.asideWidth} style='transition: width 0.5s;'>
+          <div class='rt-dashboard-mixin__brand'>
+            ROBOT
+          </div>
+          <div class='rt-dashboard-mixin__menu'>
+            <Menu menu={this.menu} collapse={this.menuCollapse} />
+          </div>
+          <div class='rt-dashboard-mixin__collapse'>
+            <i class='el-icon-menu' onClick={this.toggleCollapse} />
+          </div>
+        </ElAside>
+      )
+    },
+    toggleCollapse () {
+      this.menuCollapse = !this.menuCollapse
+    }
+  },
+  data () {
+    return {
+      collapseData: false
+    }
+  },
+  computed: {
+    /**
+     * @public
+     */
+    menu () {
+      return [
+        {
+          id: 1,
+          title: '首页',
+          router: { name: 'homepage' },
+          icon: 'el-icon-menu'
+        },
+        {
+          id: 2,
+          title: '产品模块',
+          icon: 'el-icon-tickets',
+          children: [
+            {
+              id: 3,
+              title: '产品列表',
+              router: { name: 'products' }
+            }
+          ]
+        },
+        {
+          id: 4,
+          title: '用户模块',
+          router: { name: 'homepage' },
+          icon: 'el-icon-setting'
+        }
+      ]
+    },
+    /**
+     * @public
+     */
+    menuCollapse: {
+      get () {
+        return this.collapseData
+      },
+      set (value) {
+        this.collapseData = value
+      }
+    },
+    asideWidth () {
+      return this.menuCollapse ? '64px' : '200px'
+    }
+  }
 }
+
+export default config => mixComponents(config, Dashboard)
