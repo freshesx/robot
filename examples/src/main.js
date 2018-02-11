32ddb0f4c9f462363e7974928d049d3b4190e4c1
ChaselWu <minowu@foxmail.com>
2018-02-11 16:50:49 +0800

    Update example main.

--- examples/src/main.js
+++ examples/src/main.js
@@ -1,18 +1,18 @@
 import Vue from 'vue'
 import { Robot } from '@freshes/robot'
-import router from './router'
-import store from './store'
 import app from './app'
+// --
+import dashboard from './components/dashboard'
+import sign from './components/sign'
+// ---
 import '../../dist/main.css'
 import 'element-ui/lib/theme-chalk/index.css'
 
-Vue.use(new Robot())
-
 Vue.config.productionTip = process.env.NODE_ENV !== 'production'
 
-new Vue({
-  el: '#app',
-  store,
-  router,
-  render: h => h(app)
+const robot = new Robot({
+  dashboard,
+  sign
 })
+
+robot.render(Vue, app)
