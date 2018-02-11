f31f43ce29039ee57349f85f22e9f9e83d1aff09
ChaselWu <minowu@foxmail.com>
2018-02-11 13:07:20 +0800

    Add todo

--- examples/src/components/dashboard/router.js
+++ examples/src/components/dashboard/router.js
@@ -1,6 +1,7 @@
 import homepage from '../homepage/router.js'
 import products from '../products/router.js'
 
+// @todo 定义一个新的 router 类，用于控制输入
 export default [
   {
     path: '/dashboard',
--- examples/src/components/dashboard/store.js
+++ examples/src/components/dashboard/store.js
@@ -1,5 +1,6 @@
 import menu from './menu.js'
 
+// @todo 一个新的 store 模块类，用于约定如何命名 state 和 mutations
 export default {
   state: {
     menu
