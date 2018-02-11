090176d85f6e17e5a24f4eab628745df86ab844b
ChaselWu <minowu@foxmail.com>
2018-02-11 12:34:39 +0800

    Update example.

--- examples/src/components/sign/in.js
+++ examples/src/components/sign/in.js
@@ -1,3 +1,11 @@
 import { mixSignIn } from '@freshes/robot'
 
-export default mixSignIn()
+export default mixSignIn({
+  methods: {
+    onSubmit (event) {
+      event.preventDefault()
+      const { username, password } = this.$store.state.$robot.signIn
+      window.alert(`用户名：${username}；密码：${password}。`)
+    }
+  }
+})
