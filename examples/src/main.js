a0563413d8374678bb46cc1836cd03308033bace
ChaselWu <minowu@foxmail.com>
2018-02-11 17:22:25 +0800

    Simplify main.js.

--- examples/src/main.js
+++ examples/src/main.js
@@ -1,17 +1,14 @@
-import Vue from 'vue'
 import { Robot } from '@freshes/robot'
-// --
 import dashboard from './components/dashboard'
 import sign from './components/sign'
-// ---
-import '../../dist/main.css'
 import 'element-ui/lib/theme-chalk/index.css'
-
-Vue.config.productionTip = process.env.NODE_ENV !== 'production'
+import '../../dist/main.css'
 
 const robot = new Robot({
   dashboard,
   sign
 })
 
-robot.render(Vue)
+robot.render({
+  strict: process.env.NODE_ENV !== 'production'
+})
