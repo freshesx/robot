72826da7c653555af9e5d37933cace4dda9059a6
ChaselWu <minowu@foxmail.com>
2018-02-11 12:58:20 +0800

    Add router

--- examples/src/components/sign/in.js
+++ examples/src/components/sign/in.js
@@ -6,6 +6,7 @@ export default mixSignIn({
       event.preventDefault()
       const { username, password } = this.$store.state.$robot.signIn
       window.alert(`用户名：${username}；密码：${password}。`)
+      this.$router.push({ name: 'homepage' })
     }
   }
 })
