80b5353a48dc33f0a1857fb04f7c10dd29432fe1
ChaselWu <minowu@foxmail.com>
2018-02-12 19:18:06 +0800

    Add name param.

--- src/class/Bundle.js
+++ src/class/Bundle.js
@@ -3,11 +3,16 @@
 import type { BundleInterface } from './ModuleInterface.js'
 
 export default class Bundle implements BundleInterface {
-  constructor (params: {
+  constructor (name: string, params: {
     state: Object | void,
     commits: Object | void,
     routes: Array<mixed> | void
   } = {}) {
+    if (!name || typeof name !== 'string') {
+      throw new Error('The Bundle name does not declare.')
+    }
+
+    this.name = name
     this.state = params.state || {}
     this.commits = params.commits || {}
     this.routes = params.routes || []
