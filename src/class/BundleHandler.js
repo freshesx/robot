ed5057d1f3c3759f1b7ec44080bd6a5454ab1110
ChaselWu <minowu@foxmail.com>
2018-02-12 22:51:04 +0800

    Update BundleHandler bundle name.

--- src/class/BundleHandler.js
+++ src/class/BundleHandler.js
@@ -18,7 +18,7 @@ export default class BundleHander implements BundleHandlerInterface {
 
       const bundle = bundles[name]
 
-      bundleNames.push(name)
+      bundleNames.push(bundle.name)
 
       storeMaps[name] = {
         state: bundle.state,
