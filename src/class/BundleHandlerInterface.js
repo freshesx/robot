be47afa5fb6cef22f1e87de26a5a558d58800888
ChaselWu <minowu@foxmail.com>
2018-02-19 14:26:08 +0800

    Delete old files.

--- src/class/BundleHandlerInterface.js
+++
@@ -1,8 +0,0 @@
-import type { BundleInterface } from './BundleInterface.js'
-
-export interface BundleHanderInterface {
-  bundleNames: Array<string>,
-  storeMaps: Object,
-  routeModules: Array<mixed>,
-  constructor(bundles: Array<BundleInterface>): void
-}
