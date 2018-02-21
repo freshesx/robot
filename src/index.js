9bc6c497ea86c91b7aa7dd178417672af2c699e4
ChaselWu <minowu@foxmail.com>
2018-02-21 21:23:03 +0800

    Fix export bug.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,4 @@
 import Robot from './class/Robot'
-import Bundle from './class/Bundle'
-import mapRobotStore from './helpers/mapRobotStore.js'
 import mix from './helpers/mix.js'
 import mixDashboard from './mixins/dashboard/mixDashboard.js'
 import mixJumbotron from './mixins/jumbotron/mixJumbotron.js'
@@ -10,8 +8,6 @@ import mixPasswordInput from './mixins/form/mixPasswordInput.js'
 
 export {
   Robot,
-  Bundle,
-  mapRobotStore,
   mix,
   mixDashboard,
   mixJumbotron,
