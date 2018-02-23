8efd3fd3166dc92b346264364c558aec2312dbc1
ChaselWu <minowu@foxmail.com>
2018-02-23 10:29:12 +0800

    Set appEnvs.

--- scripts/house/development.js
+++ scripts/house/development.js
@@ -2,5 +2,8 @@ const DevelopmentRunner = require('@freshes/house/src/Runners/DevelopmentRunner'
 
 new DevelopmentRunner()
   .setOptions(require('./options'))
+  .setAppEnvs({
+    NODE_ENV: 'development'
+  })
   .extend(require('./extend'))
   .run()
--- scripts/house/production.js
+++ scripts/house/production.js
@@ -2,5 +2,8 @@ const ProductionRunner = require('@freshes/house/src/Runners/ProductionRunner')
 
 new ProductionRunner()
   .setOptions(require('./options'))
+  .setAppEnvs({
+    NODE_ENV: 'production'
+  })
   .extend(require('./extend'))
   .run()
