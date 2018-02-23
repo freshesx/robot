8dc8f2efe5d0a82039c121ba98045803f3ceb2c6
ChaselWu <minowu@foxmail.com>
2018-02-23 11:20:29 +0800

    Add mixSetting.

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import mix from './helpers/mix.js'
 import mixDashboard from './mixins/dashboard/mixDashboard.js'
 import mixJumbotron from './mixins/jumbotron/mixJumbotron.js'
 import mixSignIn from './mixins/sign/mixSignIn.js'
+import mixSetting from './mixins/setting/mixSetting.js'
 import mixTextInput from './mixins/form/mixTextInput.js'
 import mixPasswordInput from './mixins/form/mixPasswordInput.js'
 
@@ -12,6 +13,7 @@ export {
   mixDashboard,
   mixJumbotron,
   mixSignIn,
+  mixSetting,
   mixTextInput,
   mixPasswordInput
 }
---
+++ src/mixins/setting/mixSetting.js
@@ -0,0 +1,15 @@
+import mix from '../../helpers/mix.js'
+// import {
+//   Container
+// } from 'element-ui'
+
+const Setting = {
+  name: 'RtSetting',
+  render (h) {
+    return (
+      <div>Hello world!</div>
+    )
+  }
+}
+
+export default config => mix(Setting, config)
